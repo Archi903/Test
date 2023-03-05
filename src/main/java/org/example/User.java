c2fbdeac03f0082e08a30daccbfe2ba5928fbe24
Archi903 <artpele@yandex.ru>
2023-03-05 19:34:51 +0300

    Ready

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,26 @@
-package org.example;public class User {
+package org.example;
+
+public class User {
+
+    private String login;
+    private String email;
+
+    public User(String login,String email) {
+        this.login = login;
+        this.email = email;
+    }
+    public User(){
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+
 }
--- src/test/java/example/UserTest.java
+++ src/test/java/example/UserTest.java
@@ -1,2 +1,39 @@
-package example;public class UserTest {
+package example;
+
+import org.example.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    private static final String CORRECT_LOGIN = "login";
+    private static final String CORRECT_EMAIL = "login@mail.ru";
+    private static final String CORRECT_ADDRESS = "@mail.ru";
+
+
+    @Test
+    public void addTwoParameters (){
+        User user = new User(CORRECT_LOGIN,CORRECT_EMAIL);
+        Assertions.assertEquals(CORRECT_LOGIN,user.getLogin());
+        Assertions.assertEquals(CORRECT_EMAIL,user.getEmail());
+    }
+
+    @Test
+    public void addNoParameters (){
+        User user = new User();
+        Assertions.assertNull(user.getLogin());
+        Assertions.assertNull(user.getEmail());
+    }
+
+    @Test
+    public void correctTypeOfEmailAddress(){
+        User user = new User(CORRECT_LOGIN, CORRECT_EMAIL);
+        Assertions.assertTrue(user.getEmail().contains(CORRECT_ADDRESS));
+    }
+
+    @Test
+    public void checkThatLoginAndEmailNotSame(){
+        User user = new User(CORRECT_LOGIN, CORRECT_EMAIL);
+        Assertions.assertNotEquals(user.getEmail(), user.getLogin());
+    }
 }
